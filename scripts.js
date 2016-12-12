3401d28deeb8afe5c781a7810aac9e9fac73913a
Iyad87 <iyad0tata@gmail.com>
2016-12-12 16:56:57 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
 $(function() {
   
         $.ajax({
+          type:'GET'
             url: '/http http://swapi.co/api/starships/9/
 ',
-            
+            success:function(data){
+          console.log('success',data);
+        }
             
            });
 });
